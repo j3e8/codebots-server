7992158b8169787ab325e7d8ef2731a164aba713
Jake Jenne <jake.jenne@nuvi.com>
2019-11-07 16:08:14 -0700

    added data to the scan payload

--- src/bot/functions/scan.js
+++ src/bot/functions/scan.js
@@ -6,14 +6,26 @@ module.exports = function(callback) {
       bots: bot.match.bots.map((b) => {
         return {
           id: b.id,
+          hp: b.hp,
           location: {
             x: b.location.x,
             y: b.location.y,
-            rotation: b.rotation,
-            velocity: b.velocity
-          }
+          },
+          rotation: b.rotation,
+          velocity: b.velocity,
         }
-      })
+      }),
+      bullets: bot.match.bullets.map((b) => {
+        return {
+          damage: b.damage,
+          location: {
+            x: b.location.x,
+            y: b.location.y,
+          },
+          angle: b.angle,
+          velocity: b.velocity,
+        };
+      }),
     }
     callback(results);
   }, bot.scanDuration);
