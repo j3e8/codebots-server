45c182653cc599e99d4580c5027c6d73a71ddddd
Jake Jenne <jake.jenne@nuvi.com>
2019-12-13 13:57:10 -0700

    splice instructions when bot is not alive

--- src/match/process-action.js
+++ src/match/process-action.js
@@ -1,9 +1,16 @@
 module.exports = function processAction(instr, elapsedMs) {
+  // If the bot isn't alive, don't process the instruction and splice it out of the queue
+  if (!instr.bot.alive) {
+    // remove the instruction from the queue
+    this.instructionQueue.splice(this.instructionQueue.indexOf(instr), 1);
+    return;
+  }
+
   let result = instr.instruction(elapsedMs);
 
   // call the animate frame method
   if (result.finished) {
-    // remove the instruction from the queue
+    // If the action is finished, remove the instruction from the queue
     this.instructionQueue.splice(this.instructionQueue.indexOf(instr), 1);
 
     // call the callback if there is one
